6f581166aa96c18544e410d8bf09dc8e115d3428
Tony <adeglio@gmail.com>
2016-10-17 13:48:34 -0400

    Remove event working.

--- Back/removeEvent.php
+++ Back/removeEvent.php
@@ -4,27 +4,24 @@
 
 	if (isset($_POST['event_id'])) {
 
-	$input = $_POST;
-
-	$sql = "DELETE FROM EVENTS WHERE event_id=" . $_POST["event_id"] . ";";
+	$sql = "DELETE FROM EVENTS WHERE event_id = " . $_POST["event_id"] . ";";
 
 	$response = array(
-			"sql" => $sql,
-			"post" => $input
+			"sql" => $sql
 		);
 
-/*
-		if (mysqli_query($link, $sql_edit)) {
+
+		if (mysqli_query($link, $sql) && mysql_affected_rows() > 0) {
 			// echo "Event updated successfully";
 			$response = array( "status" => 200,
-				"msg" => "Event Deleted!");
+				"msg" => "Event Deleted!",
+				"sql" => $sql );
 		} else {
 			// echo "Error: " . $sql_edit . "<br>" . mysqli_error($link);
 			$response = array( "status" => 404,
-				"msg" => "Error Deleting Event!" );
+				"msg" => "Error Deleting Event!",
+				"sql" => $sql );
 		}
-*/
-	}
 
 		echo json_encode($response);
 
